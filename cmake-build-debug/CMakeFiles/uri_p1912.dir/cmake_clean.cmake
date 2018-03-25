e508d0ad805f10ea84723de2fbef86a60a04a601
Boombarm <boombarm@gmail.com>
2018-03-25 16:30:02 +0700

    25032018

---
+++ cmake-build-debug/CMakeFiles/uri_p1912.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uri_p1912.dir/URI/test.cpp.obj"
+  "uri_p1912.pdb"
+  "uri_p1912.exe"
+  "uri_p1912.exe.manifest"
+  "liburi_p1912.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uri_p1912.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/uri_p1912.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for uri_p1912.
+# This may be replaced when dependencies are built.
